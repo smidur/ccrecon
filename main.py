8fb0f8b66d2b4e05e2f50d4e11aa2d0861b37b51
Roman W. (smidur) <vromabz@gmail.com>
2024-06-01 06:30:25 +0300

    reformat design; reorganize find value section

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import streamlit as st
 from backend import JCOps, PCOps, find_value
 from opera_trx_codes.trx_codes_extractor import trx_codes
@@ -43,89 +44,110 @@ with input_tab:
     #     file_upload_status = True
     # else:
     #     st.warning(file_upload_warning_txt)
-
-with tables_tab:
     if not file_upload_status:
         st.warning(file_upload_warning_txt)
     else:
-        jc = JCOps('20240427_jc.txt', rate)
-        pc = PCOps('20240427_pc.xlsx', rate)
+        jc_obj = JCOps('20240427_jc.txt', rate)
+        pc_obj = PCOps('20240427_pc.xlsx', rate)
 
-        with st.expander('**Tables\' Properties**', expanded=True):
-            with st.form(key='properties_form'):
-                jc_settings, pc_settings = st.columns(2)
-                with jc_settings:
-                    st.subheader('**Journal by CC table properties**')
+        with st.form(key='properties_form'):
+            jc_settings, pc_settings = st.columns(2)
+            with jc_settings:
+                st.subheader('**Journal by Credit Card table properties**')
 
-                    jc_brand_col, jc_trx_type_col, jc_currency_col, jc_pos_col = st.columns(4)
-                    with jc_brand_col:
-                        jc_brand = st.selectbox('Brand name', options=BRANDS, key='jc_brand')
-                    with jc_trx_type_col:
-                        jc_trx_types = ['Online + Offline', 'Online', 'Offline']
-                        jc_trx_type = st.selectbox('Trx type', options=jc_trx_types)
-                    with jc_currency_col:
-                        jc_currency = st.selectbox('Currency', options=CURRENCIES, key='jc_currency')
-                    with jc_pos_col:
-                        jc_pos_setup = st.selectbox('POS / No POS', options=POS_SETUPS, key='jc_pos_setup')
+                jc_brand_col, jc_trx_type_col, jc_currency_col, jc_pos_col = st.columns(4)
+                with jc_brand_col:
+                    jc_brand = st.selectbox('Brand name', options=BRANDS, key='jc_brand')
+                with jc_trx_type_col:
+                    jc_trx_types = ['Online + Offline', 'Online', 'Offline']
+                    jc_trx_type = st.selectbox('Trx type', options=jc_trx_types)
+                with jc_currency_col:
+                    jc_currency = st.selectbox('Currency', options=CURRENCIES, key='jc_currency')
+                with jc_pos_col:
+                    jc_pos_setup = st.selectbox('POS / No POS', options=POS_SETUPS, key='jc_pos_setup')
 
-                    jc_all_columns = jc.jc.columns
-                    jc_default_columns = ['trx_time', 'guest_name', 'note',
-                                          'room', 'currency', 'ils', 'usd']
-                    jc_columns_choice = st.multiselect('Set desired columns', options=jc_all_columns,
-                                                       default=jc_default_columns)
+                jc_all_columns = jc_obj.jc.columns
+                jc_default_columns = ['trx_time', 'guest_name', 'note',
+                                      'room', 'currency', 'ils', 'usd']
+                jc_columns_choice = st.multiselect('Set desired columns', options=jc_all_columns,
+                                                   default=jc_default_columns)
 
-                with pc_settings:
-                    st.subheader('**Pele card table properties**')
+            with pc_settings:
+                st.subheader('**Pele card table properties**')
 
-                    pc_brand_col, pc_currency_col, pc_pos_col = st.columns(3)
-                    with pc_brand_col:
-                        pc_brand = st.selectbox('Brand name', options=BRANDS, key='pc_brand')
-                    with pc_currency_col:
-                        pc_currency = st.selectbox('Currency', options=CURRENCIES, key='pc_currency')
-                    with pc_pos_col:
-                        pc_pos_setup = st.selectbox('POS / No POS', options=POS_SETUPS, key='pc_pos_setup')
+                pc_brand_col, pc_currency_col, pc_pos_col = st.columns(3)
+                with pc_brand_col:
+                    pc_brand = st.selectbox('Brand name', options=BRANDS, key='pc_brand')
+                with pc_currency_col:
+                    pc_currency = st.selectbox('Currency', options=CURRENCIES, key='pc_currency')
+                with pc_pos_col:
+                    pc_pos_setup = st.selectbox('POS / No POS', options=POS_SETUPS, key='pc_pos_setup')
 
-                    pc_all_columns = pc.pc.columns
-                    pc_default_columns = ['usd', 'ils', 'card_num', 'trx_time',
-                                          'note', 'currency', 'appr_num']
-                    pc_columns_choice = st.multiselect('Set desired columns', options=pc_all_columns,
-                                                       default=pc_default_columns)
+                pc_all_columns = pc_obj.pc.columns
+                pc_default_columns = ['usd', 'ils', 'card_num', 'trx_time',
+                                      'note', 'currency', 'appr_num']
+                pc_columns_choice = st.multiselect('Set desired columns', options=pc_all_columns,
+                                                   default=pc_default_columns)
 
-                apply_button = st.form_submit_button('APPLY', use_container_width=True)
+            apply_button = st.form_submit_button('APPLY', use_container_width=True)
 
-        with st.form(key='find_form'):
-            jc_find_col, pc_find_col = st.columns(2)
+with (tables_tab):
+    st.subheader('Search forms')
+    with st.form(key='find_form'):
+        jc_find_col, pc_find_col = st.columns(2)
+        with jc_find_col:
+            jc_find_options_col, jc_find_value_col = st.columns(2)
+            with jc_find_options_col:
+                jc_find_multiselect_options = jc_columns_choice.copy()
+                jc_find_multiselect_options = list(reversed(jc_find_multiselect_options))
+                jc_find_options_default_index = jc_find_multiselect_options.index('note')
+                jc_find_options = st.selectbox('Find in column:', options=jc_find_multiselect_options,
+                                               index=jc_find_options_default_index,
+                                               label_visibility='visible')
 
-            find_options_col, find_value_col, find_reset_button_col = st.columns([7, 10, 1])
-            with find_options_col:
-                find_multiselect_options = jc_columns_choice.copy()
-                find_multiselect_options.insert(0, 'all')
-                find_options = st.selectbox('Find in column:', options=jc_columns_choice, label_visibility='visible')
+            with jc_find_value_col:
+                jc_find_value_input = st.text_input('Value:', max_chars=200,
+                                                    label_visibility='visible', key='jc_find_value')
 
-            with find_value_col:
-                find_value_input = st.text_input('Value:', max_chars=200, label_visibility='visible', key='find_value')
+    with pc_find_col:
+        pc_find_options_col, pc_find_value_col = st.columns(2)
+        with pc_find_options_col:
+            pc_find_multiselect_options = pc_columns_choice.copy()
+            pc_find_options_default_index = pc_find_multiselect_options.index('card_num')
+            pc_find_options = st.selectbox('Find in column:', options=pc_find_multiselect_options,
+                                           index=pc_find_options_default_index,
+                                           label_visibility='visible')
 
-            with find_reset_button_col:
-                st.write('')
-                st.write('')
-                find_reset_button = st.form_submit_button('RESET')
+        with pc_find_value_col:
+            pc_find_value_input = st.text_input('Value:', max_chars=200, label_visibility='visible',
+                                                key='pc_find_value')
 
+        find_submit_col, find_reset_col = st.columns([2, 1])
+        with find_submit_col:
             find_submit_button = st.form_submit_button('FIND', use_container_width=True)
+        with find_reset_col:
+            find_reset_button = st.form_submit_button('RESET', use_container_width=True)
 
-        if find_reset_button:
-            find_submit_button = False
-            reset_find_button = False
+    if find_reset_button:
+        find_submit_button = False
+        reset_find_button = False
 
-        jc_table_col, pc_table_col = st.columns(2)
-        with jc_table_col:
-            if find_submit_button and find_options not in [None, '']:
-                jc_table: object = find_value(df=jc.jc, column_name=find_options, value=find_value_input)
-                discovered = st.data_editor(jc_table, height=2000, num_rows='dynamic',
-                                            hide_index=True, disabled=False, use_container_width=True)
-            else:
-                jc_table = st.data_editor(jc.set_columns(jc_columns_choice), height=2000, num_rows='dynamic',
-                                          hide_index=True, disabled=False, use_container_width=True)
-
-        with pc_table_col:
-            pc_table = st.data_editor(pc.set_columns(pc_columns_choice), height=2000, num_rows='dynamic',
+    jc_table_col, pc_table_col = st.columns(2)
+    with jc_table_col:
+        if find_submit_button and jc_find_options not in [None, '']:
+            jc_table: pd.DataFrame = find_value(df=jc_obj.jc, column_name=jc_find_options, value=jc_find_value_input)
+            jc_discovered = st.data_editor(jc_table, height=2000, num_rows='dynamic',
+                                           hide_index=True, disabled=False, use_container_width=True)
+        else:
+            jc_table = st.data_editor(jc_obj.set_columns(jc_columns_choice), height=2000, num_rows='dynamic',
                                       hide_index=True, disabled=False, use_container_width=True)
+
+    with pc_table_col:
+        if find_submit_button and pc_find_options not in [None, '']:
+            pc_table: pd.DataFrame = find_value(df=pc_obj.pc, column_name=pc_find_options, value=pc_find_value_input)
+            pc_discovered = st.data_editor(pc_table, height=2000, num_rows='dynamic',
+                                           hide_index=True, disabled=False, use_container_width=True)
+        else:
+            pc_table = st.data_editor(pc_obj.set_columns(pc_columns_choice), height=2000, num_rows='dynamic',
+                                      hide_index=True, disabled=False, use_container_width=True)
+
